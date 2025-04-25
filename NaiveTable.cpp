f0199b98fc5d062e93c1dc1f828f500758d49c90
Haris Khawja <hariskhawja@gmail.com>
2025-04-24 20:26:53 -0400

    Naive Table implementation

---
+++ NaiveTable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include "HashTable.h"
+#include "NaiveTable.h"
+
+// Constructors
+NaiveTable::NaiveTable() : HashTable() {};
+NaiveTable::NaiveTable(size_t S) : HashTable(S) {};
+
+NaiveTable::~NaiveTable() {}; // Destructor
+
+// Naive Hash Function: char_sum % table_size
+int NaiveTable::hash(std::string val) const {
+    size_t sum = std::accumulate(val.begin(), val.end(), 0);
+
+    return sum % getTableSize();
+}
---
+++ NaiveTable.h
@@ -0,0 +1,22 @@
+#ifndef NAIVETABLE_H
+#define NAIVETABLE_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include "HashTable.h"
+
+// Hash Table Implementation With a Naive Hash Function: char_sum % table_size
+class NaiveTable : public HashTable {
+    public:
+        NaiveTable(); // Constructor
+        NaiveTable(size_t S); // Constructor with User-Defined Slot Number
+        
+    protected:
+        virtual ~NaiveTable(); // Destructor
+
+    private:
+        virtual int hash(std::string key) const override;
+};
+
+#endif
